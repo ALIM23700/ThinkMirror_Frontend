86cc373c8540890c0529dba87403b3c6d452b6e3
abdul alim <abdulalim23700@gmail.com>
2026-05-18 21:04:42 +0600

    slove localhost problem

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
       setLoading(true);
 
       const res = await axios.post(
-        "http://localhost:5000/api/analyze",
+        "https://thinkmirror-backend.onrender.com/api/analyze",
         { thought }
       );
 
